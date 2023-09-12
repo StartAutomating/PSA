b9b0d288447885fe6a26a8e0fee87fa2eed43b95
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:07 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Block-AtGraphActor.ps1
@@ -0,0 +1,56 @@
+function Block-AtGraphActor  {
+<#
+.Synopsis
+    app.bsky.graph.muteActor
+.Description
+    app.bsky.graph.muteActor
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/muteActor.json
+#>
+[Alias('PSA.app.bsky.graph.muteActor')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('actor')]
+[String]
+$Actor
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.muteActor'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
