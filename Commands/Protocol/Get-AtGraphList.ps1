473c9767693b73b146ea156bf0512223fb875040
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:07 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtGraphList.ps1
+++ Commands/Protocol/Get-AtGraphList.ps1
@@ -26,6 +26,7 @@ begin {
 $NamespaceID = 'app.bsky.graph.getList'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
