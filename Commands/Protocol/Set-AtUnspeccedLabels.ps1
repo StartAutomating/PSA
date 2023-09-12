dff2e307c703fa8b70c9c3b5e37075da879a4f90
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:09 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Set-AtUnspeccedLabels.ps1
+++ Commands/Protocol/Set-AtUnspeccedLabels.ps1
@@ -19,6 +19,7 @@ begin {
 $NamespaceID = 'app.bsky.unspecced.applyLabels'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
