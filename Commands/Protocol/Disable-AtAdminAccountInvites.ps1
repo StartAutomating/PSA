6265c3774d329fe26fbebb0d6b68ae9414a56cbc
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:04 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Disable-AtAdminAccountInvites.ps1
@@ -0,0 +1,60 @@
+function Disable-AtAdminAccountInvites  {
+<#
+.Synopsis
+    com.atproto.admin.disableAccountInvites
+.Description
+    com.atproto.admin.disableAccountInvites
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/admin/disableAccountInvites.json
+#>
+[Alias('PSA.com.atproto.admin.disableAccountInvites')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('account')]
+[String]
+$Account,
+# Additionally add a note describing why the invites were disabled
+[ComponentModel.DefaultBindingProperty('note')]
+[String]
+$Note
+)
+
+begin {
+$NamespaceID = 'com.atproto.admin.disableAccountInvites'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
