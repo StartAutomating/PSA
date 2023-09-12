c4b790d4cdb05fd713ed3ba479537a8042391068
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:09 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Search-AtActors.ps1
+++ Commands/Protocol/Search-AtActors.ps1
@@ -25,6 +25,7 @@ begin {
 $NamespaceID = 'app.bsky.actor.searchActors'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
