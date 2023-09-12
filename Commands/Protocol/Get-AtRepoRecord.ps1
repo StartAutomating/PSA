c370fca72c635c0e3d1c975ae883c06b0d0b2260
StartAutomating <StartAutomating@github.com>
2023-09-12 22:54:03 +0000

    Updating PSA Build (Enabling -ContentType) (Fixes #26)

--- Commands/Protocol/Get-AtRepoRecord.ps1
+++ Commands/Protocol/Get-AtRepoRecord.ps1
@@ -37,6 +37,7 @@ $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
+$AsByte = $false
 
 
     
@@ -66,7 +67,13 @@ end {
             $parameterQueue.ToArray() |
                 Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
                     $_
-                } -ParameterAlias $parameterAliases @InvokeAtSplat
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
         
 }
 } 
