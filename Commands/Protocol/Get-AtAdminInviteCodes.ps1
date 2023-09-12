f4d8a28798d537d7879937d9e0482daeb92d6aae
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:04 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtAdminInviteCodes.ps1
+++ Commands/Protocol/Get-AtAdminInviteCodes.ps1
@@ -25,6 +25,7 @@ begin {
 $NamespaceID = 'com.atproto.admin.getInviteCodes'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
