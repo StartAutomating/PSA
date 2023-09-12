653c2bb4699a402c566358d94aadcc7c554be2a1
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:08 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtFeedActorLikes.ps1
+++ Commands/Protocol/Get-AtFeedActorLikes.ps1
@@ -26,6 +26,7 @@ begin {
 $NamespaceID = 'app.bsky.feed.getActorLikes'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
