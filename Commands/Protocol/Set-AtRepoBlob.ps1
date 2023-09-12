1e4ff2ec35dd46b70c04f883122817409f8ad60e
StartAutomating <StartAutomating@github.com>
2023-09-12 22:54:04 +0000

    Updating PSA Build (Enabling -ContentType) (Fixes #26)

--- Commands/Protocol/Set-AtRepoBlob.ps1
+++ Commands/Protocol/Set-AtRepoBlob.ps1
@@ -10,7 +10,13 @@ function Set-AtRepoBlob  {
 [Alias('PSA.com.atproto.repo.uploadBlob')]
 [CmdletBinding(SupportsShouldProcess)]
 param(
-
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('.')]
+[Byte[]]
+$Data,
+[Parameter(ValueFromPipelineByPropertyName)]
+[String]
+$ContentType
 )
 
 begin {
@@ -19,6 +25,21 @@ $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
 
 
     $parameterQueue = [Collections.Queue]::new()
@@ -34,7 +55,13 @@ end {
             $parameterQueue.ToArray() |
                 Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
                     $_
-                } -ParameterAlias $parameterAliases @InvokeAtSplat
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
         
 }
 } 
