d783a88d0d6eca867ed8d2c2f88757823a192dd7
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:07 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Block-AtGraphActor.ps1
+++ Commands/Protocol/Block-AtGraphActor.ps1
@@ -19,6 +19,7 @@ begin {
 $NamespaceID = 'app.bsky.graph.muteActor'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
