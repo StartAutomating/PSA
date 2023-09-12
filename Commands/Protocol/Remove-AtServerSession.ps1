d6b446c499d3384b6b818e5406223783ec9791d0
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:05 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Remove-AtServerSession.ps1
@@ -0,0 +1,40 @@
+function Remove-AtServerSession  {
+<#
+.Synopsis
+    com.atproto.server.deleteSession
+.Description
+    com.atproto.server.deleteSession
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/server/deleteSession.json
+#>
+[Alias('PSA.com.atproto.server.deleteSession')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+
+)
+
+begin {
+$NamespaceID = 'com.atproto.server.deleteSession'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
