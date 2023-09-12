6f03190303062dc23cf7f65333e4a8e0a4ac419e
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:07 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtGraphListMutes.ps1
+++ Commands/Protocol/Get-AtGraphListMutes.ps1
@@ -22,6 +22,7 @@ begin {
 $NamespaceID = 'app.bsky.graph.getListMutes'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
