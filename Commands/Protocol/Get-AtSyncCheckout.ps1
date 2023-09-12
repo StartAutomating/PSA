0164b4b9bb67eb89c78e40c9e2416345e6af49b2
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:03 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtSyncCheckout.ps1
+++ Commands/Protocol/Get-AtSyncCheckout.ps1
@@ -21,6 +21,7 @@ begin {
 $NamespaceID = 'com.atproto.sync.getCheckout'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
