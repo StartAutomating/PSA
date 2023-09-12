d40cb9c57ce2559a50dae894a8f909430e215d2e
StartAutomating <StartAutomating@github.com>
2023-09-12 22:54:06 +0000

    Updating PSA Build (Enabling -ContentType) (Fixes #26)

--- Commands/Protocol/Sync-AtServerSession.ps1
+++ Commands/Protocol/Sync-AtServerSession.ps1
@@ -19,6 +19,7 @@ $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
+$AsByte = $false
 
 
     $parameterQueue = [Collections.Queue]::new()
@@ -34,7 +35,13 @@ end {
             $parameterQueue.ToArray() |
                 Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
                     $_
-                } -ParameterAlias $parameterAliases @InvokeAtSplat
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
         
 }
 } 
