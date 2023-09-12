5c82ec9fe9acaaa77609d5da22c7353dc87bffa4
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:05 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Search-AtAdminRepos.ps1
@@ -0,0 +1,65 @@
+function Search-AtAdminRepos  {
+<#
+.Synopsis
+    com.atproto.admin.searchRepos
+.Description
+    com.atproto.admin.searchRepos
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/admin/searchRepos.json
+#>
+[Alias('PSA.com.atproto.admin.searchRepos')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('term')]
+[String]
+$Term,
+[ComponentModel.DefaultBindingProperty('invitedBy')]
+[String]
+$InvitedBy,
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'com.atproto.admin.searchRepos'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
