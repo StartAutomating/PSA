1af4f68786a6beb5c593703cd5003a057c050bb4
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:06 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Get-AtNotificationUnreadCount.ps1
@@ -0,0 +1,56 @@
+function Get-AtNotificationUnreadCount  {
+<#
+.Synopsis
+    app.bsky.notification.getUnreadCount
+.Description
+    app.bsky.notification.getUnreadCount
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/notification/getUnreadCount.json
+#>
+[Alias('PSA.app.bsky.notification.getUnreadCount')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('seenAt')]
+[String]
+$SeenAt
+)
+
+begin {
+$NamespaceID = 'app.bsky.notification.getUnreadCount'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
