ca3f7c93d3123fd107946e2f49ff5f2ab054b2e5
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:03 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Add-AtModerationReport.ps1
@@ -0,0 +1,62 @@
+function Add-AtModerationReport  {
+<#
+.Synopsis
+    com.atproto.moderation.createReport
+.Description
+    com.atproto.moderation.createReport
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/moderation/createReport.json
+#>
+[Alias('PSA.com.atproto.moderation.createReport')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('reasonType')]
+[Management.Automation.PSObject]
+$ReasonType,
+[ComponentModel.DefaultBindingProperty('reason')]
+[String]
+$Reason,
+[ComponentModel.DefaultBindingProperty('subject')]
+[Management.Automation.PSObject]
+$Subject
+)
+
+begin {
+$NamespaceID = 'com.atproto.moderation.createReport'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
