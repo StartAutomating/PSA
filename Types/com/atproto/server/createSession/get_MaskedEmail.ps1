cd6b584bf6b5dfac993c4d1393ec3fc44519ed9c
James Brundage <@github.com>
2023-09-12 02:16:48 -0700

    Adding com.atproto.server.createSession.get_MaskedEmail (Fixes #22)
    Adding help

--- Types/com/atproto/server/createSession/get_MaskedEmail.ps1
+++ Types/com/atproto/server/createSession/get_MaskedEmail.ps1
@@ -1,1 +1,7 @@
-
+<#
+.SYNOPSIS
+    Masks an email
+.DESCRIPTION
+    Masks an email, displaying only the first 3 letters of the address and domain.
+#>
+$this.email -replace '(?<u>.{3}).+?@(?<d>.{3}).+?(?<s>\..+)$','${u}***@${d}***${s}'
