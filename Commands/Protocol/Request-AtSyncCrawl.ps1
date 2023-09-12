d5c574d4c64cdb30c6e427046cb9bbfc4ed1c936
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:04 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Request-AtSyncCrawl.ps1
@@ -0,0 +1,57 @@
+function Request-AtSyncCrawl  {
+<#
+.Synopsis
+    com.atproto.sync.requestCrawl
+.Description
+    com.atproto.sync.requestCrawl
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/sync/requestCrawl.json
+#>
+[Alias('PSA.com.atproto.sync.requestCrawl')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# Hostname of the service that is requesting to be crawled.
+[ComponentModel.DefaultBindingProperty('hostname')]
+[String]
+$Hostname
+)
+
+begin {
+$NamespaceID = 'com.atproto.sync.requestCrawl'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
