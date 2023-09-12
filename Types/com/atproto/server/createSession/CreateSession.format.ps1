8e51b23c595bfc5de3df98643e3eeeace6a6bec6
James Brundage <@github.com>
2023-09-12 02:22:19 -0700

    Adding com.atproto.server.createSession JWT views (Fixes #23)

--- Types/com/atproto/server/createSession/CreateSession.format.ps1
+++ Types/com/atproto/server/createSession/CreateSession.format.ps1
@@ -4,3 +4,13 @@ Write-FormatView -TypeName com.atproto.server.createSession -Property handle, em
         $_.MaskedEmail
     }
 }
+
+Write-FormatView -TypeName com.atproto.server.createSession -Property handle, email, did -VirtualProperty @{
+    email = {
+        $_.MaskedEmail
+    }
+} -AsList
+
+Write-FormatView -TypeName com.atproto.server.createSession -Property accessJWT, refreshJWT -Name JWT
+
+Write-FormatView -TypeName com.atproto.server.createSession -Property accessJWT, refreshJWT -Name JWT -AsList
