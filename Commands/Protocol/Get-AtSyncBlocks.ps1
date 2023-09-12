787fa297c79c7dec6133a3b96ee349472593df0c
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:03 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtSyncBlocks.ps1
+++ Commands/Protocol/Get-AtSyncBlocks.ps1
@@ -25,6 +25,7 @@ begin {
 $NamespaceID = 'com.atproto.sync.getBlocks'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
