9aedfd1fe7e3069a7ab2ca24879206d5b7dd79f3
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:07 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Get-AtGraphListMutes.ps1
@@ -0,0 +1,59 @@
+function Get-AtGraphListMutes  {
+<#
+.Synopsis
+    app.bsky.graph.getListMutes
+.Description
+    app.bsky.graph.getListMutes
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/getListMutes.json
+#>
+[Alias('PSA.app.bsky.graph.getListMutes')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.getListMutes'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
