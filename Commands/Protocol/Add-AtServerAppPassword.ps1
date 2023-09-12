5c7af7ebc63b788feb0f0eee841ba0e4aae4237f
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:05 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Add-AtServerAppPassword.ps1
+++ Commands/Protocol/Add-AtServerAppPassword.ps1
@@ -19,6 +19,7 @@ begin {
 $NamespaceID = 'com.atproto.server.createAppPassword'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
