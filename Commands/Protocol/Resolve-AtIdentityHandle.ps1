43b24eab418cf150150df744a676d7a18121447b
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:02 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Resolve-AtIdentityHandle.ps1
+++ Commands/Protocol/Resolve-AtIdentityHandle.ps1
@@ -21,6 +21,7 @@ begin {
 $NamespaceID = 'com.atproto.identity.resolveHandle'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
