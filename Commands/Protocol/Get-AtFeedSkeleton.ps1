373faecd5525eefc475c0db264a27f8bef86cd02
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:08 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtFeedSkeleton.ps1
+++ Commands/Protocol/Get-AtFeedSkeleton.ps1
@@ -26,6 +26,7 @@ begin {
 $NamespaceID = 'app.bsky.feed.getFeedSkeleton'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
