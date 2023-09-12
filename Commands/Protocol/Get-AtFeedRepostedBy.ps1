bd3e7b9e0caee72fa7a60ed8f3af094adb8df1eb
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:08 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Get-AtFeedRepostedBy.ps1
@@ -0,0 +1,66 @@
+function Get-AtFeedRepostedBy  {
+<#
+.Synopsis
+    app.bsky.feed.getRepostedBy
+.Description
+    app.bsky.feed.getRepostedBy
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/feed/getRepostedBy.json
+#>
+[Alias('PSA.app.bsky.feed.getRepostedBy')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('uri')]
+[String]
+$Uri,
+[ComponentModel.DefaultBindingProperty('cid')]
+[String]
+$Cid,
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'app.bsky.feed.getRepostedBy'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
