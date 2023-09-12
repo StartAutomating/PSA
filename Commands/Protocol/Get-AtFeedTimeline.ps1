e1c35257fd1b2b8dabeae89c40d59bb992152dd9
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:08 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtFeedTimeline.ps1
+++ Commands/Protocol/Get-AtFeedTimeline.ps1
@@ -25,6 +25,7 @@ begin {
 $NamespaceID = 'app.bsky.feed.getTimeline'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
