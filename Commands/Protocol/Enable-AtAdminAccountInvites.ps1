a667085639e1241562c0e82aa96f3de18a41495b
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:04 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Enable-AtAdminAccountInvites.ps1
+++ Commands/Protocol/Enable-AtAdminAccountInvites.ps1
@@ -23,6 +23,7 @@ begin {
 $NamespaceID = 'com.atproto.admin.enableAccountInvites'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
