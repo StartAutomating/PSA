a07ad35b272cc05f0fd9ec3db7f250a27c9707ed
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:06 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Update-AtTempRepoVersion.ps1
@@ -0,0 +1,59 @@
+function Update-AtTempRepoVersion  {
+<#
+.Synopsis
+    com.atproto.temp.upgradeRepoVersion
+.Description
+    com.atproto.temp.upgradeRepoVersion
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/temp/upgradeRepoVersion.json
+#>
+[Alias('PSA.com.atproto.temp.upgradeRepoVersion')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('did')]
+[String]
+$Did,
+[ComponentModel.DefaultBindingProperty('force')]
+[Management.Automation.SwitchParameter]
+$Force
+)
+
+begin {
+$NamespaceID = 'com.atproto.temp.upgradeRepoVersion'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
