ca57310fe2b3ff352888bbd89bee64d994071876
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:08 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Get-AtActorProfiles.ps1
@@ -0,0 +1,57 @@
+function Get-AtActorProfiles  {
+<#
+.Synopsis
+    app.bsky.actor.getProfiles
+.Description
+    app.bsky.actor.getProfiles
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/actor/getProfiles.json
+#>
+[Alias('PSA.app.bsky.actor.getProfiles')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('actors')]
+[Management.Automation.PSObject]
+$Actors
+)
+
+begin {
+$NamespaceID = 'app.bsky.actor.getProfiles'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
