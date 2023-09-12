32853b6129ccd4b5e6d999715be3ab91cd2b3775
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:05 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Undo-AtAdminModerationAction.ps1
+++ Commands/Protocol/Undo-AtAdminModerationAction.ps1
@@ -25,6 +25,7 @@ begin {
 $NamespaceID = 'com.atproto.admin.reverseModerationAction'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
