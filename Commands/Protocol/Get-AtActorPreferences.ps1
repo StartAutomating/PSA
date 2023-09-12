5a53e2838f0868ee7acabf146bef26d498919d82
StartAutomating <StartAutomating@github.com>
2023-09-12 07:52:08 +0000

    Adding PSA PipeScript Build (Fixes #6)
    This will generate a PowerShell command for every part of the AtProtocol Lexicon.

---
+++ Commands/Protocol/Get-AtActorPreferences.ps1
@@ -0,0 +1,40 @@
+function Get-AtActorPreferences  {
+<#
+.Synopsis
+    app.bsky.actor.getPreferences
+.Description
+    app.bsky.actor.getPreferences
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/actor/getPreferences.json
+#>
+[Alias('PSA.app.bsky.actor.getPreferences')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+
+)
+
+begin {
+$NamespaceID = 'app.bsky.actor.getPreferences'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$parameterAliases = [Ordered]@{}
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat
+        
+}
+} 
+
