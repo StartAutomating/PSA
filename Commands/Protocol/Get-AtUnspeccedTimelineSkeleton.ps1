f4a9e64e33c7938e5f6c20a5c4ba726e6a8d4b39
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:09 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtUnspeccedTimelineSkeleton.ps1
+++ Commands/Protocol/Get-AtUnspeccedTimelineSkeleton.ps1
@@ -22,6 +22,7 @@ begin {
 $NamespaceID = 'app.bsky.unspecced.getTimelineSkeleton'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
