48a04c5e47fbcb744f7caa2777be656c09847ade
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:05 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Update-AtAdminAccountHandle.ps1
+++ Commands/Protocol/Update-AtAdminAccountHandle.ps1
@@ -22,6 +22,7 @@ begin {
 $NamespaceID = 'com.atproto.admin.updateAccountHandle'
 $httpMethod  = 'POST'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
