2d5b28a4605444d4eba07f41b3763e87d00317a9
StartAutomating <StartAutomating@github.com>
2023-09-12 08:05:04 +0000

    Decorating At Protocol Returns (Fixes #6)
    Re #7

--- Commands/Protocol/Get-AtAdminModerationAction.ps1
+++ Commands/Protocol/Get-AtAdminModerationAction.ps1
@@ -20,6 +20,7 @@ begin {
 $NamespaceID = 'com.atproto.admin.getModerationAction'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 
 
