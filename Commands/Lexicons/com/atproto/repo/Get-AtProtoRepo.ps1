027d682138a30b0d7936e2f3e8167a931b984422
StartAutomating <StartAutomating@github.com>
2023-09-13 06:21:33 +0000

    Aliasing more of the At Protocol (Fixes #28)

---
+++ Commands/Lexicons/com/atproto/repo/Get-AtProtoRepo.ps1
@@ -0,0 +1,66 @@
+function Get-AtProtoRepo  {
+<#
+.Synopsis
+    com.atproto.repo.describeRepo
+.Description
+    com.atproto.repo.describeRepo
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/repo/describeRepo.json
+#>
+[Alias('Get-AtProtocolRepo','atproto.repo.describeRepo','com.atproto.repo.describeRepo')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# The handle or DID of the repo.
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('repo')]
+[String]
+$Repo
+)
+
+begin {
+$NamespaceID = 'com.atproto.repo.describeRepo'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
