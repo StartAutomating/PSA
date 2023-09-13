bc290d01d73dd4c0fddf387dfd265b1dd480e34c
StartAutomating <StartAutomating@github.com>
2023-09-13 06:21:33 +0000

    Aliasing more of the At Protocol (Fixes #28)

---
+++ Commands/Lexicons/com/atproto/sync/Get-AtProtoSyncLatestCommit.ps1
@@ -0,0 +1,66 @@
+function Get-AtProtoSyncLatestCommit  {
+<#
+.Synopsis
+    com.atproto.sync.getLatestCommit
+.Description
+    com.atproto.sync.getLatestCommit
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/sync/getLatestCommit.json
+#>
+[Alias('Get-AtProtocolSyncLatestCommit','atproto.sync.getLatestCommit','com.atproto.sync.getLatestCommit')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# The DID of the repo.
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('did')]
+[String]
+$Did
+)
+
+begin {
+$NamespaceID = 'com.atproto.sync.getLatestCommit'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
