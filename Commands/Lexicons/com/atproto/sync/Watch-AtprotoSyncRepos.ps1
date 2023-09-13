50347cfd263f977ae38cbafc8bae788bf894aa2d
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:37 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/com/atproto/sync/Watch-AtprotoSyncRepos.ps1
@@ -0,0 +1,65 @@
+function Watch-AtprotoSyncRepos  {
+<#
+.Synopsis
+    com.atproto.sync.subscribeRepos
+.Description
+    com.atproto.sync.subscribeRepos
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/sync/subscribeRepos.json
+#>
+[Alias('PSA.com.atproto.sync.subscribeRepos')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# The last known event to backfill from.
+[ComponentModel.DefaultBindingProperty('cursor')]
+[Management.Automation.PSObject]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'com.atproto.sync.subscribeRepos'
+$httpMethod  = ''
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
