c12651deafd3fa8c237ff8af74ac1b407d5fc449
StartAutomating <StartAutomating@github.com>
2023-09-13 06:21:34 +0000

    Aliasing more of the At Protocol (Fixes #28)

---
+++ Commands/Lexicons/com/atproto/admin/Get-AtProtoAdminModerationReport.ps1
@@ -0,0 +1,65 @@
+function Get-AtProtoAdminModerationReport  {
+<#
+.Synopsis
+    com.atproto.admin.getModerationReport
+.Description
+    com.atproto.admin.getModerationReport
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/admin/getModerationReport.json
+#>
+[Alias('Get-AtProtocolAdminModerationReport','atproto.admin.getModerationReport','com.atproto.admin.getModerationReport')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('id')]
+[Management.Automation.PSObject]
+$Id
+)
+
+begin {
+$NamespaceID = 'com.atproto.admin.getModerationReport'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
