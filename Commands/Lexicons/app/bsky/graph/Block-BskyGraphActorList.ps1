88ada23ae2cc4bf8a1fdf9cb17f861d80593d915
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:40 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/app/bsky/graph/Block-BskyGraphActorList.ps1
@@ -0,0 +1,64 @@
+function Block-BskyGraphActorList  {
+<#
+.Synopsis
+    app.bsky.graph.muteActorList
+.Description
+    app.bsky.graph.muteActorList
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/muteActorList.json
+#>
+[Alias('PSA.app.bsky.graph.muteActorList')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('list')]
+[String]
+$List
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.muteActorList'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
