79f45d561a74b1b8c8e44eb1da64cf8d46dea5d3
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:39 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/app/bsky/notification/Update-BskyNotificationSeen.ps1
@@ -0,0 +1,64 @@
+function Update-BskyNotificationSeen  {
+<#
+.Synopsis
+    app.bsky.notification.updateSeen
+.Description
+    app.bsky.notification.updateSeen
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/notification/updateSeen.json
+#>
+[Alias('PSA.app.bsky.notification.updateSeen')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('seenAt')]
+[String]
+$SeenAt
+)
+
+begin {
+$NamespaceID = 'app.bsky.notification.updateSeen'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
