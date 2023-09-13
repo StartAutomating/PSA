54c4a01f5b09ec4f572ec01379ca2facf51b3b72
StartAutomating <StartAutomating@github.com>
2023-09-13 06:21:33 +0000

    Aliasing more of the At Protocol (Fixes #28)

---
+++ Commands/Lexicons/com/atproto/sync/Get-AtProtoSyncHead.ps1
@@ -0,0 +1,66 @@
+function Get-AtProtoSyncHead  {
+<#
+.Synopsis
+    com.atproto.sync.getHead
+.Description
+    com.atproto.sync.getHead
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/sync/getHead.json
+#>
+[Alias('Get-AtProtocolSyncHead','atproto.sync.getHead','com.atproto.sync.getHead')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# The DID of the repo.
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('did')]
+[String]
+$Did
+)
+
+begin {
+$NamespaceID = 'com.atproto.sync.getHead'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
