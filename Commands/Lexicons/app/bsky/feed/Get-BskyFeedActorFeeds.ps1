332aef85066dce408345416a06b72bd9de45b517
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:40 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/app/bsky/feed/Get-BskyFeedActorFeeds.ps1
@@ -0,0 +1,71 @@
+function Get-BskyFeedActorFeeds  {
+<#
+.Synopsis
+    app.bsky.feed.getActorFeeds
+.Description
+    app.bsky.feed.getActorFeeds
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/feed/getActorFeeds.json
+#>
+[Alias('PSA.app.bsky.feed.getActorFeeds')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory)]
+[ComponentModel.DefaultBindingProperty('actor')]
+[String]
+$Actor,
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'app.bsky.feed.getActorFeeds'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
