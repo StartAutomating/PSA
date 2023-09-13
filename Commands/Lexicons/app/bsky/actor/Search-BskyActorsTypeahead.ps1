bdc5abe547990de7eac69b51cf0bf470ddf13bb3
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:41 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/app/bsky/actor/Search-BskyActorsTypeahead.ps1
@@ -0,0 +1,67 @@
+function Search-BskyActorsTypeahead  {
+<#
+.Synopsis
+    app.bsky.actor.searchActorsTypeahead
+.Description
+    app.bsky.actor.searchActorsTypeahead
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/actor/searchActorsTypeahead.json
+#>
+[Alias('PSA.app.bsky.actor.searchActorsTypeahead')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('term')]
+[String]
+$Term,
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit
+)
+
+begin {
+$NamespaceID = 'app.bsky.actor.searchActorsTypeahead'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
