779f2af04305c701cbaf2fd2f38929e5c308f0f6
StartAutomating <StartAutomating@github.com>
2023-09-13 06:46:03 +0000

    Improving At Protocol Piping (Fixes #29)

--- Commands/Lexicons/com/atproto/admin/Get-AtProtoAdminRepo.ps1
+++ Commands/Lexicons/com/atproto/admin/Get-AtProtoAdminRepo.ps1
@@ -10,7 +10,7 @@ function Get-AtProtoAdminRepo  {
 [Alias('Get-AtProtocolAdminRepo','atproto.admin.getRepo','com.atproto.admin.getRepo')]
 [CmdletBinding(SupportsShouldProcess)]
 param(
-[Parameter(Mandatory)]
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('did')]
 [String]
 $Did
