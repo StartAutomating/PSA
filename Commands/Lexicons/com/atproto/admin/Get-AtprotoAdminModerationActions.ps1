2c69d39b1597ce34a84692800b5f49a3859d89ad
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:37 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/com/atproto/admin/Get-AtprotoAdminModerationActions.ps1
@@ -0,0 +1,70 @@
+function Get-AtprotoAdminModerationActions  {
+<#
+.Synopsis
+    com.atproto.admin.getModerationActions
+.Description
+    com.atproto.admin.getModerationActions
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/admin/getModerationActions.json
+#>
+[Alias('PSA.com.atproto.admin.getModerationActions')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[ComponentModel.DefaultBindingProperty('subject')]
+[String]
+$Subject,
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor
+)
+
+begin {
+$NamespaceID = 'com.atproto.admin.getModerationActions'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
