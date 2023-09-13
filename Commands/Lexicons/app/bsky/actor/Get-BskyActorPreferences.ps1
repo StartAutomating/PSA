6d0fcce1c06ca7d227553367734bdd07a26451bb
StartAutomating <StartAutomating@github.com>
2023-09-13 05:40:41 +0000

    Approximating Lexicon in Command Names (Fixes #27)

---
+++ Commands/Lexicons/app/bsky/actor/Get-BskyActorPreferences.ps1
@@ -0,0 +1,48 @@
+function Get-BskyActorPreferences  {
+<#
+.Synopsis
+    app.bsky.actor.getPreferences
+.Description
+    app.bsky.actor.getPreferences
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/actor/getPreferences.json
+#>
+[Alias('PSA.app.bsky.actor.getPreferences')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+
+)
+
+begin {
+$NamespaceID = 'app.bsky.actor.getPreferences'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$AsByte = $false
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $_
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte
+        
+}
+} 
+
