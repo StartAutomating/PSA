46c301557d59d687b7a563d5da3c168ab085e3c2
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:30 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getListMutes/get_More.ps1
+++ Types/getListMutes/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.graph.getListMutes.
+    Gets the next page of results of app.bsky.graph.getListMutes -Cache.
 #>
-$this | app.bsky.graph.getListMutes
+$this | app.bsky.graph.getListMutes -Cache
 
