b9254a73b55426e91058c2dae09c932daea65045
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:29 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getPopularFeedGenerators/get_More.ps1
+++ Types/getPopularFeedGenerators/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.unspecced.getPopularFeedGenerators.
+    Gets the next page of results of app.bsky.unspecced.getPopularFeedGenerators -Cache.
 #>
-$this | app.bsky.unspecced.getPopularFeedGenerators
+$this | app.bsky.unspecced.getPopularFeedGenerators -Cache
 
