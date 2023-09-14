c573050cadcf01b13153acee5d1e42882a6840b2
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:34 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getInviteCodes/get_More.ps1
+++ Types/getInviteCodes/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of com.atproto.admin.getInviteCodes.
+    Gets the next page of results of com.atproto.admin.getInviteCodes -Cache.
 #>
-$this | com.atproto.admin.getInviteCodes
+$this | com.atproto.admin.getInviteCodes -Cache
 
