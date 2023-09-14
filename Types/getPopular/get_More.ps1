fa48f02e86a1fff71472f9e6350fcf086f367927
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:29 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getPopular/get_More.ps1
+++ Types/getPopular/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.unspecced.getPopular.
+    Gets the next page of results of app.bsky.unspecced.getPopular -Cache.
 #>
-$this | app.bsky.unspecced.getPopular
+$this | app.bsky.unspecced.getPopular -Cache
 
