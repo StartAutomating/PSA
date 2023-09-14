0df783e212b4562be9fa01f845f55ce36c71a81d
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:31 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getFeedSkeleton/get_More.ps1
+++ Types/getFeedSkeleton/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getFeedSkeleton.
+    Gets the next page of results of app.bsky.feed.getFeedSkeleton -Cache.
 #>
-$this | app.bsky.feed.getFeedSkeleton
+$this | app.bsky.feed.getFeedSkeleton -Cache
 
