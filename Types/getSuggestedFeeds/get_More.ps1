7065fe762ecb32bc6bce94b7f2cc0c612fcb94ab
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:32 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getSuggestedFeeds/get_More.ps1
+++ Types/getSuggestedFeeds/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getSuggestedFeeds.
+    Gets the next page of results of app.bsky.feed.getSuggestedFeeds -Cache.
 #>
-$this | app.bsky.feed.getSuggestedFeeds
+$this | app.bsky.feed.getSuggestedFeeds -Cache
 
