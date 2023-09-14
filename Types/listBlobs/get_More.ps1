d559477e32c5f5df8123e540128389eadf705947
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:33 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/listBlobs/get_More.ps1
+++ Types/listBlobs/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of com.atproto.sync.listBlobs.
+    Gets the next page of results of com.atproto.sync.listBlobs -Cache.
 #>
-$this | com.atproto.sync.listBlobs
+$this | com.atproto.sync.listBlobs -Cache
 
