ff791611319fcfbd5d8dd0c353be4b4c0f8b528c
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:27 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/com/atproto/sync/Get-AtProtoSyncBlocks.ps1
+++ Commands/Lexicons/com/atproto/sync/Get-AtProtoSyncBlocks.ps1
@@ -18,7 +18,10 @@ $Did,
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cids')]
 [Management.Automation.PSObject]
-$Cids
+$Cids,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache
 )
 
 begin {
@@ -63,7 +66,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
