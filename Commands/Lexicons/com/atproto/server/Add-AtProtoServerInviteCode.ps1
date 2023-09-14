675ace76d60020b9039e212a151dcd772a1a1fe1
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:26 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/com/atproto/server/Add-AtProtoServerInviteCode.ps1
+++ Commands/Lexicons/com/atproto/server/Add-AtProtoServerInviteCode.ps1
@@ -62,7 +62,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
