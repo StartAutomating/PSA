1ceec8b15b99da7411b79280e365ac63bcca3922
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:32 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getTimeline/get_More.ps1
+++ Types/getTimeline/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getTimeline.
+    Gets the next page of results of app.bsky.feed.getTimeline -Cache.
 #>
-$this | app.bsky.feed.getTimeline
+$this | app.bsky.feed.getTimeline -Cache
 
