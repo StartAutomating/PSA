de33aecbb00acd821545872de50583bca3782e8e
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:28 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/com/atproto/label/Search-AtProtoLabels.ps1
+++ Commands/Lexicons/com/atproto/label/Search-AtProtoLabels.ps1
@@ -35,7 +35,10 @@ You can provide this -Cursor to the same command with the same input to get more
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cursor')]
 [String]
-$Cursor
+$Cursor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache
 )
 
 begin {
@@ -83,7 +86,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
