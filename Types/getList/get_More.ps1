4982e7c4c7b5c0e5eb8c6c6913b5b9267e2bf167
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:30 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getList/get_More.ps1
+++ Types/getList/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.graph.getList.
+    Gets the next page of results of app.bsky.graph.getList -Cache.
 #>
-$this | app.bsky.graph.getList
+$this | app.bsky.graph.getList -Cache
 
