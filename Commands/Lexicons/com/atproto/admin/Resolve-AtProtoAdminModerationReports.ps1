618d14aa10461d242f845cc0fa09b37e37fa8ba4
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:29 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/com/atproto/admin/Resolve-AtProtoAdminModerationReports.ps1
+++ Commands/Lexicons/com/atproto/admin/Resolve-AtProtoAdminModerationReports.ps1
@@ -66,7 +66,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
