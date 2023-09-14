7b90f7a8c4942a6f1e5e491033510dab71481d52
StartAutomating <StartAutomating@github.com>
2023-09-14 23:14:49 +0000

    PSA Build - More Support (Fixes #44)
    Fixing directory

---
+++ Types/app/bsky/feed/getActorLikes/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of app.bsky.feed.getActorLikes -Cache.
+#>
+$this | app.bsky.feed.getActorLikes -Cache
+
