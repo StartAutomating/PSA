ef588a40e9e35085c486c9a385d72873cef44424
StartAutomating <StartAutomating@github.com>
2023-09-14 18:21:46 +0000

    Deeply Decorating At Protocol Commands (Fixes #39)

--- Commands/Lexicons/app/bsky/feed/Get-BskyFeedLikes.ps1
+++ Commands/Lexicons/app/bsky/feed/Get-BskyFeedLikes.ps1
@@ -40,6 +40,9 @@ begin {
 $NamespaceID = 'app.bsky.feed.getLikes'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'likes'='#like'
+}
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 $AsByte = $false
