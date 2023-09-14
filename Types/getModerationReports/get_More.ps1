0882123335f16c8fe452ef428b6b371384657a2f
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:34 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getModerationReports/get_More.ps1
+++ Types/getModerationReports/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of com.atproto.admin.getModerationReports.
+    Gets the next page of results of com.atproto.admin.getModerationReports -Cache.
 #>
-$this | com.atproto.admin.getModerationReports
+$this | com.atproto.admin.getModerationReports -Cache
 
