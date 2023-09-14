600e926a70cce3c9c135d90dce8b0e4dcbb92c84
StartAutomating <StartAutomating@github.com>
2023-09-14 23:14:48 +0000

    PSA Build - More Support (Fixes #44)
    Fixing directory

---
+++ Types/app/bsky/unspecced/getPopularFeedGenerators/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of app.bsky.unspecced.getPopularFeedGenerators -Cache.
+#>
+$this | app.bsky.unspecced.getPopularFeedGenerators -Cache
+
