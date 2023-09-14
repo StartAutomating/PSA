2c5a86b6f671386bba776482a3a9406e1501c84a
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:31 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getAuthorFeed/get_More.ps1
+++ Types/getAuthorFeed/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getAuthorFeed.
+    Gets the next page of results of app.bsky.feed.getAuthorFeed -Cache.
 #>
-$this | app.bsky.feed.getAuthorFeed
+$this | app.bsky.feed.getAuthorFeed -Cache
 
