54f09af88389ab6be3fc33f810fc56efcde4d549
StartAutomating <StartAutomating@github.com>
2023-09-14 04:36:27 +0000

    Improving PSA Help (Fixes #37)

--- Commands/Lexicons/app/bsky/feed/Get-BskyFeedTimeline.ps1
+++ Commands/Lexicons/app/bsky/feed/Get-BskyFeedTimeline.ps1
@@ -14,10 +14,18 @@ param(
 [ComponentModel.DefaultBindingProperty('algorithm')]
 [String]
 $Algorithm,
+# A limit to the number of results returned.
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('limit')]
 [Management.Automation.PSObject]
 $Limit,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cursor')]
 [String]
