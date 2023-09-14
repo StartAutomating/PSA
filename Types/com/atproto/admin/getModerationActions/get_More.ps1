060a4dc697843a82694566fcf8296b2e710724bc
StartAutomating <StartAutomating@github.com>
2023-09-14 23:14:53 +0000

    PSA Build - More Support (Fixes #44)
    Fixing directory

---
+++ Types/com/atproto/admin/getModerationActions/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of com.atproto.admin.getModerationActions -Cache.
+#>
+$this | com.atproto.admin.getModerationActions -Cache
+
