dcf9d4e2118951ee884ef609bce06d1adbd5d688
StartAutomating <StartAutomating@github.com>
2023-09-14 18:21:44 +0000

    Deeply Decorating At Protocol Commands (Fixes #39)

--- Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
@@ -32,6 +32,9 @@ begin {
 $NamespaceID = 'app.bsky.graph.getBlocks'
 $httpMethod  = 'GET'
 $InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'blocks'='app.bsky.actor.defs#profileView'
+}
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
 $AsByte = $false
