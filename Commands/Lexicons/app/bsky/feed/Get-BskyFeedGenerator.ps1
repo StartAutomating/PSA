44ac07458cc8c61eecb24f30e6530fd7245a0f39
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:24 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/app/bsky/feed/Get-BskyFeedGenerator.ps1
+++ Commands/Lexicons/app/bsky/feed/Get-BskyFeedGenerator.ps1
@@ -13,7 +13,10 @@ param(
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('feed')]
 [String]
-$Feed
+$Feed,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache
 )
 
 begin {
@@ -63,7 +66,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
