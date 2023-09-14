bd2d6047daedfdaa9c21a15eb967b6b3e9090846
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:33 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/listRecords/get_More.ps1
+++ Types/listRecords/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of com.atproto.repo.listRecords.
+    Gets the next page of results of com.atproto.repo.listRecords -Cache.
 #>
-$this | com.atproto.repo.listRecords
+$this | com.atproto.repo.listRecords -Cache
 
