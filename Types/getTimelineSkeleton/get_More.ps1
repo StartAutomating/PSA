df0d3f7349473cb77ecd51db25c074c998a85e97
StartAutomating <StartAutomating@github.com>
2023-09-14 22:54:31 +0000

    PSA Build - More Support (Fixes #44)
    Removing -Passthru

---
+++ Types/getTimelineSkeleton/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of app.bsky.unspecced.getTimelineSkeleton.
+#>
+$this | app.bsky.unspecced.getTimelineSkeleton
+
