acd0b7b006c5b6fc38a4f3090089b0fb574dd48d
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:23 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
@@ -25,7 +25,10 @@ You can provide this -Cursor to the same command with the same input to get more
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cursor')]
 [String]
-$Cursor
+$Cursor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache
 )
 
 begin {
@@ -73,7 +76,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
