5a18d69e2941239f7b2b2da5a01d05b5fc0fad0e
StartAutomating <StartAutomating@github.com>
2023-09-14 20:21:27 +0000

    PSA Build - Adding -Cache (Fixes #40)
    And carrying properties (Fixes #43)

--- Commands/Lexicons/com/atproto/repo/Set-AtProtoRepoBlob.ps1
+++ Commands/Lexicons/com/atproto/repo/Set-AtProtoRepoBlob.ps1
@@ -61,7 +61,11 @@ end {
                     } else {
                         "application/json"   
                     }
-                ) -AsByte:$AsByte
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                )
         
 }
 } 
