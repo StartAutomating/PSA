5a84dc97b10fa7eee914d57349730cbf50558084
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:31 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getLikes/get_More.ps1
+++ Types/getLikes/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getLikes.
+    Gets the next page of results of app.bsky.feed.getLikes -Cache.
 #>
-$this | app.bsky.feed.getLikes
+$this | app.bsky.feed.getLikes -Cache
 
