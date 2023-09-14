dcd7a1eeed8c9f16ffed2117e26006aff3c816fe
StartAutomating <StartAutomating@github.com>
2023-09-14 04:36:26 +0000

    Improving PSA Help (Fixes #37)

--- Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphBlocks.ps1
@@ -10,10 +10,18 @@ function Get-BskyGraphBlocks  {
 [Alias('Get-BlueSkyGraphBlocks','bsky.graph.getBlocks','app.bsky.graph.getBlocks')]
 [CmdletBinding(SupportsShouldProcess)]
 param(
+# A limit to the number of results returned.
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('limit')]
 [Management.Automation.PSObject]
 $Limit,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cursor')]
 [String]
