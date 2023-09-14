1333cb962ac7ec60ae70f3418bf65f13e67aab5b
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:32 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getRepostedBy/get_More.ps1
+++ Types/getRepostedBy/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.feed.getRepostedBy.
+    Gets the next page of results of app.bsky.feed.getRepostedBy -Cache.
 #>
-$this | app.bsky.feed.getRepostedBy
+$this | app.bsky.feed.getRepostedBy -Cache
 
