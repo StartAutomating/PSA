53ebc86a62beb92e74dc743ef192fb29f0d44d46
StartAutomating <StartAutomating@github.com>
2023-09-14 22:54:35 +0000

    PSA Build - More Support (Fixes #44)
    Removing -Passthru

---
+++ Types/getModerationReports/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of com.atproto.admin.getModerationReports.
+#>
+$this | com.atproto.admin.getModerationReports
+
