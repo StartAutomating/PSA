20d729ab6598e1e38b6d401f9ca1c31d03d81edb
StartAutomating <StartAutomating@github.com>
2023-09-14 23:14:50 +0000

    PSA Build - More Support (Fixes #44)
    Fixing directory

---
+++ Types/app/bsky/feed/getFeedSkeleton/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of app.bsky.feed.getFeedSkeleton -Cache.
+#>
+$this | app.bsky.feed.getFeedSkeleton -Cache
+
