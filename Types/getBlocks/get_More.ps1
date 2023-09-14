dace17b169b915fc0459230f70cd8fe3250605f6
StartAutomating <StartAutomating@github.com>
2023-09-14 23:10:29 +0000

    PSA Build - More Support (Fixes #44)
    Caching when possible

--- Types/getBlocks/get_More.ps1
+++ Types/getBlocks/get_More.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
     Gets additional results.
 .DESCRIPTION
-    Gets the next page of results of app.bsky.graph.getBlocks.
+    Gets the next page of results of app.bsky.graph.getBlocks -Cache.
 #>
-$this | app.bsky.graph.getBlocks
+$this | app.bsky.graph.getBlocks -Cache
 
