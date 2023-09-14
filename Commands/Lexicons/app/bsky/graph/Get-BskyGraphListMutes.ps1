ec8485bd8b9cec35419fc83d88523308fa671ade
StartAutomating <StartAutomating@github.com>
2023-09-14 04:36:26 +0000

    Improving PSA Help (Fixes #37)

--- Commands/Lexicons/app/bsky/graph/Get-BskyGraphListMutes.ps1
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphListMutes.ps1
@@ -10,10 +10,18 @@ function Get-BskyGraphListMutes  {
 [Alias('Get-BlueSkyGraphListMutes','bsky.graph.getListMutes','app.bsky.graph.getListMutes')]
 [CmdletBinding(SupportsShouldProcess)]
 param(
+# A limit to the number of results returned.
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('limit')]
 [Management.Automation.PSObject]
 $Limit,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('cursor')]
 [String]
