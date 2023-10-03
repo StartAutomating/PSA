ea214361157065b3bac72fc947799012f4eb7c07
StartAutomating <StartAutomating@github.com>
2023-10-03 01:01:00 +0000

    PSA AtProtocol Build - Adding -Raw to all commands (Fixes #74)

--- Commands/Lexicons/app/bsky/unspecced/Get-BskyUnspeccedPopularFeedGenerators.ps1
+++ Commands/Lexicons/app/bsky/unspecced/Get-BskyUnspeccedPopularFeedGenerators.ps1
@@ -32,7 +32,10 @@ $Cursor,
 $Query,
 # If set, will cache results for performance.
 [Management.Automation.SwitchParameter]
-$Cache
+$Cache,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
 )
 
 begin {
@@ -44,6 +47,7 @@ $InvokeAtSplat.DecorateProperty = [Ordered]@{
 }
 $InvokeAtSplat["PSTypeName"] = $NamespaceID
 $parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
 $AsByte = $false
 
 
@@ -54,6 +58,7 @@ $AsByte = $false
     foreach ($attr in $paramMetadata.Attributes) {
         if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
             $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
             continue nextParameter
         }
     }
@@ -73,7 +78,13 @@ end {
 
             $parameterQueue.ToArray() |
                 Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
-                    $_
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
                 } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
                     if ($ContentType) {
                         $ContentType
@@ -84,7 +95,7 @@ end {
                     $_
                 } -Cache:$(
                     if ($cache) {$cache} else { $false }
-                )
+                ) -Raw:$Raw
         
 }
 } 
