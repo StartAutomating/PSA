d2cac96f44375c54d4cdbb0837c0105d5bb8575d
StartAutomating <StartAutomating@github.com>
2023-10-03 18:56:08 +0000

    Updating at protocol build (explicitly updating submodules)

---
+++ Commands/Lexicons/com/atproto/server/Request-AtProtoServerEmailUpdate.ps1
@@ -0,0 +1,71 @@
+function Request-AtProtoServerEmailUpdate  {
+<#
+.Synopsis
+    com.atproto.server.requestEmailUpdate
+.Description
+    com.atproto.server.requestEmailUpdate
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/com/atproto/server/requestEmailUpdate.json
+#>
+[Alias('Request-AtProtocolServerEmailUpdate','atproto.server.requestEmailUpdate','com.atproto.server.requestEmailUpdate')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'com.atproto.server.requestEmailUpdate'
+$httpMethod  = 'POST'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
