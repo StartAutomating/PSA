1fac5531889d47b7f2866cd45cd7c9f6142f83b2
StartAutomating <StartAutomating@github.com>
2023-10-03 18:56:10 +0000

    Updating at protocol build (explicitly updating submodules)

---
+++ Commands/Lexicons/app/bsky/feed/Search-BskyFeedPosts.ps1
@@ -0,0 +1,110 @@
+function Search-BskyFeedPosts  {
+<#
+.Synopsis
+    app.bsky.feed.searchPosts
+.Description
+    app.bsky.feed.searchPosts
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/feed/searchPosts.json
+#>
+[Alias('Search-BlueSkyFeedPosts','bsky.feed.searchPosts','app.bsky.feed.searchPosts')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# search query string; syntax, phrase, boolean, and faceting is unspecified, but Lucene query syntax is recommended
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('q')]
+[String]
+$Q,
+# A limit to the number of results returned.
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+# optional pagination mechanism; may not necessarily allow scrolling through entire result set
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'app.bsky.feed.searchPosts'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'posts'='app.bsky.feed.defs#postView'
+    'posts.author'='app.bsky.actor.defs#profileViewBasic'
+    'posts.viewer'='app.bsky.feed.defs#viewerState'
+    'posts.threadgate'='app.bsky.feed.defs#threadgateView'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
