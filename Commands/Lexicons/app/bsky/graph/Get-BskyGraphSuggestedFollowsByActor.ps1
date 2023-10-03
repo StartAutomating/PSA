7ab56eb2d0ae5b9c8d5c1e605f321e5620e558c0
StartAutomating <StartAutomating@github.com>
2023-10-03 18:56:11 +0000

    Updating at protocol build (explicitly updating submodules)

---
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphSuggestedFollowsByActor.ps1
@@ -0,0 +1,101 @@
+function Get-BskyGraphSuggestedFollowsByActor  {
+<#
+.Synopsis
+    app.bsky.graph.getSuggestedFollowsByActor
+.Description
+    app.bsky.graph.getSuggestedFollowsByActor
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/getSuggestedFollowsByActor.json
+#>
+[Alias('Get-BlueSkyGraphSuggestedFollowsByActor','bsky.graph.getSuggestedFollowsByActor','app.bsky.graph.getSuggestedFollowsByActor')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+<#
+The Actor.
+
+This can be either a handle (e.g. @AtProto.com) or a Decentralized Identifier (.did)
+#>
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('actor')]
+[String]
+$Actor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.getSuggestedFollowsByActor'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'suggestions'='app.bsky.actor.defs#profileView'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
