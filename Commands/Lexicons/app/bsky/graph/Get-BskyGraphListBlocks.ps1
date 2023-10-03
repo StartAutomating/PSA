e45e3b041be9c5ec8de23b318ce377357858dbc0
StartAutomating <StartAutomating@github.com>
2023-10-03 18:56:10 +0000

    Updating at protocol build (explicitly updating submodules)

---
+++ Commands/Lexicons/app/bsky/graph/Get-BskyGraphListBlocks.ps1
@@ -0,0 +1,111 @@
+function Get-BskyGraphListBlocks  {
+<#
+.Synopsis
+    app.bsky.graph.getListBlocks
+.Description
+    app.bsky.graph.getListBlocks
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/getListBlocks.json
+#>
+[Alias('Get-BlueSkyGraphListBlocks','bsky.graph.getListBlocks','app.bsky.graph.getListBlocks')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# A limit to the number of results returned.
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.getListBlocks'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'lists'='app.bsky.graph.defs#listView'
+    'lists.creator'='app.bsky.actor.defs#profileView'
+    'lists.purpose'='app.bsky.graph.defs#listPurpose'
+    'lists.viewer'='app.bsky.graph.defs#listViewerState'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
