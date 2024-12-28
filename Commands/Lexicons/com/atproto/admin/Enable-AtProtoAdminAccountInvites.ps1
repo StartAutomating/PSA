3326c783da92db7c7d596c8937b4f42ee88f53fd
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:40 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

--- Commands/Lexicons/com/atproto/admin/Enable-AtProtoAdminAccountInvites.ps1
+++ Commands/Lexicons/com/atproto/admin/Enable-AtProtoAdminAccountInvites.ps1
@@ -14,7 +14,7 @@ param(
 [ComponentModel.DefaultBindingProperty('account')]
 [String]
 $Account,
-# Additionally add a note describing why the invites were enabled
+# Optional reason for enabled invites.
 [Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('note')]
 [String]
