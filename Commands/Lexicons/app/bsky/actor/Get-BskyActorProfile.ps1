b3b368f47816e1ab51ff30234c76449183ea738d
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:34 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

--- Commands/Lexicons/app/bsky/actor/Get-BskyActorProfile.ps1
+++ Commands/Lexicons/app/bsky/actor/Get-BskyActorProfile.ps1
@@ -10,11 +10,7 @@ function Get-BskyActorProfile  {
 [Alias('Get-BlueSkyActorProfile','bsky.actor.getProfile','app.bsky.actor.getProfile')]
 [CmdletBinding(SupportsShouldProcess)]
 param(
-<#
-The Actor.
-
-This can be either a handle (e.g. @AtProto.com) or a Decentralized Identifier (.did)
-#>
+# Handle or DID of account to fetch profile of.
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('actor')]
 [String]
