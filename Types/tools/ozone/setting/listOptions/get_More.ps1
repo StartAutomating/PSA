e9877e31613813ff2a3c5934015b45c25452ef1c
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:36 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

---
+++ Types/tools/ozone/setting/listOptions/get_More.ps1
@@ -0,0 +1,9 @@
+
+<#
+.SYNOPSIS
+    Gets additional results.
+.DESCRIPTION
+    Gets the next page of results of tools.ozone.setting.listOptions -Cache.
+#>
+$this | tools.ozone.setting.listOptions -Cache
+
