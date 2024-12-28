96c4d4d60bbc6f445f695fa62eb6badee2fce4f4
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:36 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

---
+++ Commands/Lexicons/app/bsky/graph/Search-BskyGraphStarterPacks.ps1
@@ -0,0 +1,124 @@
+function Search-BskyGraphStarterPacks  {
+<#
+.Synopsis
+    app.bsky.graph.searchStarterPacks
+.Description
+    app.bsky.graph.searchStarterPacks
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/app/bsky/graph/searchStarterPacks.json
+#>
+[Alias('Search-BlueSkyGraphStarterPacks','bsky.graph.searchStarterPacks','app.bsky.graph.searchStarterPacks')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# Search query string. Syntax, phrase, boolean, and faceting is unspecified, but Lucene query syntax is recommended.
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('q')]
+[String]
+$Q,
+# A limit to the number of results returned.
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'app.bsky.graph.searchStarterPacks'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'starterPacks'='app.bsky.graph.defs#starterPackViewBasic'
+    'starterPacks.creator'='app.bsky.actor.defs#profileViewBasic'
+    'starterPacks.creator.associated'='app.bsky.actor.defs#profileAssociated'
+    'starterPacks.creator.associated.chat'='app.bsky.actor.defs#profileAssociatedChat'
+    'starterPacks.creator.viewer'='app.bsky.graph.defs#viewerState'
+    'starterPacks.creator.viewer.mutedByList'='app.bsky.graph.defs#listViewBasic'
+    'starterPacks.creator.viewer.mutedByList.purpose'='app.bsky.graph.defs#listPurpose'
+    'starterPacks.creator.viewer.mutedByList.viewer'='app.bsky.graph.defs#listViewerState'
+    'starterPacks.creator.viewer.blockingByList'='app.bsky.graph.defs#listViewBasic'
+    'starterPacks.creator.viewer.blockingByList.purpose'='app.bsky.graph.defs#listPurpose'
+    'starterPacks.creator.viewer.blockingByList.viewer'='app.bsky.graph.defs#listViewerState'
+    'starterPacks.creator.viewer.knownFollowers'='app.bsky.graph.defs#knownFollowers'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
