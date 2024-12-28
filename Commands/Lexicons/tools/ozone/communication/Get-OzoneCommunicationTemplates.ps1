38e0b24678488af95f892235de7ae18b4160329f
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:38 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

---
+++ Commands/Lexicons/tools/ozone/communication/Get-OzoneCommunicationTemplates.ps1
@@ -0,0 +1,77 @@
+function Get-OzoneCommunicationTemplates  {
+<#
+.Synopsis
+    tools.ozone.communication.listTemplates
+.Description
+    tools.ozone.communication.listTemplates
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/tools/ozone/communication/listTemplates.json
+#>
+[Alias('ozone.communication.listTemplates','tools.ozone.communication.listTemplates')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'tools.ozone.communication.listTemplates'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'communicationTemplates'='tools.ozone.communication.defs#templateView'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
