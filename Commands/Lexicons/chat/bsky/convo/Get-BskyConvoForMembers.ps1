ff2fe84a5476a3bdbb1bb4d9197fca2a60cb79a5
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:38 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

---
+++ Commands/Lexicons/chat/bsky/convo/Get-BskyConvoForMembers.ps1
@@ -0,0 +1,96 @@
+function Get-BskyConvoForMembers  {
+<#
+.Synopsis
+    chat.bsky.convo.getConvoForMembers
+.Description
+    chat.bsky.convo.getConvoForMembers
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/chat/bsky/convo/getConvoForMembers.json
+#>
+[Alias('Get-BlueSkyConvoForMembers','bsky.convo.getConvoForMembers','chat.bsky.convo.getConvoForMembers')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('members')]
+[Management.Automation.PSObject]
+$Members,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'chat.bsky.convo.getConvoForMembers'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'convo'='chat.bsky.convo.defs#convoView'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
