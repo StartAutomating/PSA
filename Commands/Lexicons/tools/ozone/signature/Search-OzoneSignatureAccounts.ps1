39a3cfbfb58a3fe6b76b232897e9ec9a6f0b3104
StartAutomating <StartAutomating@github.com>
2024-12-28 07:38:36 +0000

    feat: Rebuilding from AtProtocol and making build conditional ( Fixes #88, Fixes #89 )

---
+++ Commands/Lexicons/tools/ozone/signature/Search-OzoneSignatureAccounts.ps1
@@ -0,0 +1,112 @@
+function Search-OzoneSignatureAccounts  {
+<#
+.Synopsis
+    tools.ozone.signature.searchAccounts
+.Description
+    tools.ozone.signature.searchAccounts
+.Link
+    https://github.com/bluesky-social/atproto/tree/main/lexicons/tools/ozone/signature/searchAccounts.json
+#>
+[Alias('ozone.signature.searchAccounts','tools.ozone.signature.searchAccounts')]
+[CmdletBinding(SupportsShouldProcess)]
+param(
+[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('values')]
+[Management.Automation.PSObject]
+$Values,
+<#
+A cursor that can be used to get more results.
+
+Any command that accepts a -Cursor parameter returns a .Cursor property.
+
+You can provide this -Cursor to the same command with the same input to get more results.
+#>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('cursor')]
+[String]
+$Cursor,
+# A limit to the number of results returned.
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('limit')]
+[Management.Automation.PSObject]
+$Limit,
+# If set, will cache results for performance.
+[Management.Automation.SwitchParameter]
+$Cache,
+# The authorization. This can be a JWT that accesses the at protocol or a credential. If this is provided as a credential the username is a handle or email and the password is the app password.
+[Alias('Authentication','AppPassword','Credential','PSCredential')]
+[Management.Automation.SwitchParameter]
+$Authorization,
+# If set, will return raw results. This will ignore -Property, -DecorateProperty, -ExpandProperty, and -PSTypeName.
+[Management.Automation.SwitchParameter]
+$Raw
+)
+
+begin {
+$NamespaceID = 'tools.ozone.signature.searchAccounts'
+$httpMethod  = 'GET'
+$InvokeAtSplat = [Ordered]@{Method=$httpMethod}
+$InvokeAtSplat.DecorateProperty = [Ordered]@{
+    'accounts'='com.atproto.admin.defs#accountView'
+}
+$InvokeAtSplat["PSTypeName"] = $NamespaceID
+$parameterAliases = [Ordered]@{}
+$DataboundParameters = @()
+$AsByte = $false
+
+
+    
+:nextParameter foreach ($paramMetadata in 
+    ([Management.Automation.CommandMetadata]$MyInvocation.MyCommand).Parameters.Values) {
+    
+    foreach ($attr in $paramMetadata.Attributes) {
+        if ($attr -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+            $parameterAliases[$paramMetadata.Name] = $attr.Name
+            $DataboundParameters += $paramMetadata.Name
+            continue nextParameter
+        }
+    }
+}
+
+
+
+    $parameterQueue = [Collections.Queue]::new()
+
+}
+process {
+
+$parameterQueue.Enqueue([Ordered]@{} + $PSBoundParameters)            
+        
+}
+end {
+
+            $parameterQueue.ToArray() |
+                Invoke-AtProtocol -Method $httpMethod -NamespaceID $NamespaceID -Parameter {
+                    $RestParameters =[Ordered]@{}
+                    foreach ($parameterName in $DataboundParameters) {
+                        if ($null -ne $_.($ParameterName)) {
+                            $RestParameters[$parameterName] = $_.($ParameterName)
+                        }
+                    }
+                    $RestParameters
+                } -ParameterAlias $parameterAliases @InvokeAtSplat -ContentType $(
+                    if ($ContentType) {
+                        $ContentType
+                    } else {
+                        "application/json"   
+                    }
+                ) -AsByte:$AsByte -Property {
+                    $_
+                } -Cache:$(
+                    if ($cache) {$cache} else { $false }
+                ) -Raw:$Raw -Authorization {
+                    if ($_.Authorization) { 
+                        $_.Authorization
+                    } else { 
+                        $null
+                    }
+                }
+        
+}
+} 
+
